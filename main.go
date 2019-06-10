8d6cba10c99114d73f29182aff5f277039f67960
ritwik310 <ritwiksahaofficial@gmail.com>
2019-06-10 21:50:29 +0530

    Ufff, It worked, Tracker request worked! (Super unorganised)

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"net/url"
 	"os"
-	"reflect"
+	"time"
 
+	bencoding "github.com/marksamman/bencode"
 	"github.com/ritwik310/torrent-client/bencode"
 )
+
+func infohash(info interface{}) []byte {
+	enc := bencoding.Encode(info)
+	h := sha1.New()
+	h.Write(enc)
+	hash := h.Sum(nil)
+
+	return hash
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -20,20 +32,96 @@ func main() {
 		fmt.Println("couldn't read/decode the torrent file data", err)
 	}
 
-	var pieces string
+	for k := range torr {
+		fmt.Println("Key:", k)
+	}
+
+	infohash := infohash(torr["info"])
+	peerID := string(time.Now().Unix())
+	port := "6889"
+	uploaded := "0"
+	downloaded := "0"
+	var left int64
+	compact := "1"
+
+	// info := torr["info"]
 
 	if info, ok := torr["info"].(map[string]interface{}); ok {
 		for key, val := range info {
-			if key == "pieces" {
-				fmt.Println("original type of info[\"pieces\"]:", reflect.TypeOf(val))
-				pieces = val.(string)
+			// fmt.Println(key, ":")
+			// fmt.Printf("\n%+v\n\n", val)
+
+			if key == "piece length" {
+				left = val.(int64)
 			}
+
+			// if key == "pieces" && false {
+			// 	fmt.Println("original type of info[\"pieces\"]:", reflect.TypeOf(val))
+			// 	// pieces = val.(string)
+			// }
 		}
 
 		// fmt.Printf("%s\n", y["pieces"])
 	}
 
-	fmt.Println("len:", len(pieces))
-	fmt.Printf("%#x\n", pieces)
+	fmt.Printf("info_hash: %#x\n", infohash)
+	fmt.Printf("peer_id: %v\n", peerID)
+	fmt.Printf("port: %v\n", port)
+	fmt.Printf("uploaded: %v\n", uploaded)
+	fmt.Printf("downloaded: %v\n", downloaded)
+	fmt.Printf("left: %v\n", left)
+	fmt.Printf("left: %v\n", compact)
+
+	var Url *url.URL
+	Url, err = url.Parse("http://bt1.archive.org:6969")
+	if err != nil {
+		panic("boom")
+	}
+
+	Url.Path += "/announce"
+	parameters := url.Values{}
+	parameters.Add("infohash", string(infohash))
+	parameters.Add("peerID", peerID)
+	parameters.Add("port", port)
+	parameters.Add("uploaded", uploaded)
+	parameters.Add("downloaded", downloaded)
+	parameters.Add("left", string(left))
+	parameters.Add("compact", compact)
+	Url.RawQuery = parameters.Encode()
+
+	fmt.Printf("Encoded URL is %q\n", Url.String())
+
+	return
+
+	// params := map[string]string{
+	// 	"info_hash": string(h.Sum(nil)),
+	// }
+
+	// fmt.Println("params:", params)
+
+	// return
+
+	// fmt.Printf("%+v\n", torr["info"])
+
+	// return
+
+	// var pieces string
+
+	// if info, ok := torr["info"].(map[string]interface{}); ok {
+	// 	for key, val := range info {
+	// 		fmt.Println(key, ":")
+	// 		fmt.Printf("\n%+v\n\n", val)
+
+	// 		if key == "pieces" && false {
+	// 			fmt.Println("original type of info[\"pieces\"]:", reflect.TypeOf(val))
+	// 			pieces = val.(string)
+	// 		}
+	// 	}
+
+	// 	// fmt.Printf("%s\n", y["pieces"])
+	// }
+
+	// fmt.Println("len:", len(pieces))
+	// fmt.Printf("%#x\n", pieces)
 
 }
--- play/main.go
+++ play/main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 
-	tracker := "http://bt1.archive.org:6969/announce"
+	// "http://torrent.ubuntu.com:6969/announce?info_hash=%90%28%9F%D3M%FC%1C%F8%F3%16%A2h%AD%D85L%853DX&peer_id=-PC0001-706887310628&uploaded=0&downloaded=0&left=699400192&port=6889&compact=1"
+
+	// params := map[string]string{
+
+	// }
+
+	tracker := "http://bt1.archive.org:6969/announce?compact=1&downloaded=0&infohash=%5E3tV%85%8DUj%14%D5%9Be%AE1%2CrH%E6%BA%C7&left=%F2%80%80%80&peerID=%EF%BF%BD&port=6889&uploaded=0"
 	resp, err := http.Get(tracker)
 
 	// conn, err := net.Dial("tcp", "[http://torrent.ubuntu.com]:6969/announce")
